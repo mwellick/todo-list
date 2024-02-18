12dcd038bd14578e2fcd5b5b1ae81961931afad6
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-02-18 19:38:28 +0200

    Updated index.html and settings.py(added crispy forms)

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "todo",
+    "crispy_forms",
+    "crispy_bootstrap4",
+
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
