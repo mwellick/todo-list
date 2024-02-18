5c71f496228772d64409e35d9fa09567d49068bf
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-02-18 19:37:32 +0200

    Added TaskCreateView and url

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, TaskListView, TaskCreateView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Tag, Task
@@ -17,3 +18,11 @@ def index(request: HttpRequest) -> HttpResponse:
 
 class TaskListView(generic.ListView):
     model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    template_name = "todo/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
